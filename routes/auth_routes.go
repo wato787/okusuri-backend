a8c1684a2482a0f51da76f6f51603f9491a340fe
wato787 <wato787@icloud.com>
2025-04-11 21:44:25 +0900

    ar

---
+++ routes/auth_routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"okusuri-backend/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterAuthRoutes(router *gin.RouterGroup, authController *controller.AuthController) {
+	auth := router.Group("/auth")
+	{
+		auth.POST("/signup", authController.Signup)
+		// 認証が必要なルート
+		// authorized := auth.Group("/")
+		// authorized.Use(middleware.JWTAuth())
+		{
+			// authorized.POST("/logout", controllers.Logout)
+		}
+	}
+}
