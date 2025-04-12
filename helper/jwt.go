7cb35f63bcfa61b20dde392601ea36633f2df164
wato787 <wato787@icloud.com>
2025-04-12 19:37:25 +0900

    any

--- helper/jwt.go
+++ helper/jwt.go
@@ -23,7 +23,7 @@ func ValidateToken(tokenString string) (uint, error) {
 	}
 
 	// トークンを解析
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
