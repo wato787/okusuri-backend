1c32b0ef3cf7c2255bec41fb7df1002f2833a04b
wato787 <wato787@icloud.com>
2025-04-12 16:41:27 +0900

    service仮実装

--- service/auth_service.go
+++ service/auth_service.go
@@ -1,23 +1,48 @@
 package service
 
 import (
-	"log"
+	"okusuri-backend/dto"
+	"okusuri-backend/model"
 	"okusuri-backend/repository"
 )
 
-// AuthService は認証関連の機能を提供する
 type AuthService struct {
 	userRepo *repository.UserRepository
 }
 
-// NewAuthService は新しいAuthServiceのインスタンスを作成する
 func NewAuthService(userRepo *repository.UserRepository) *AuthService {
 	return &AuthService{
 		userRepo: userRepo,
 	}
 }
 
-// RegisterUser は新規ユーザーを登録する
-func (s *AuthService) RegisterUser(email, password string) {
-	log.Println("RegisterUser called")
+func (s *AuthService) RegisterUser(req dto.SignupRequest) (*model.User, error) {
+	// IDトークンの検証
+
+	// ユーザーの存在確認
+	existingUser, err := s.userRepo.FindByProviderId(req.ProviderID)
+	if err != nil {
+		return nil, err
+	}
+
+	if existingUser != nil {
+		return nil, nil // ユーザーは既に存在する
+	}
+
+	// 新しいユーザーの作成
+	newUser := &model.User{
+		Email:      req.Email,
+		Name:       req.Name,
+		ImageUrl:   req.ImageURL,
+		Provider:   model.ProviderGoogle,
+		ProviderId: req.ProviderID,
+	}
+
+	// ユーザーの保存
+	if err := s.userRepo.Create(newUser); err != nil {
+		return nil, err
+	}
+
+	return newUser, nil
+
 }
