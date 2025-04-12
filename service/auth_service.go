a4c6c4bdec45e4afcd50ff7ce2149403bbdbc6b7
wato787 <wato787@icloud.com>
2025-04-12 16:28:14 +0900

    不要ファイル削除

--- service/auth_service.go
+++ service/auth_service.go
@@ -1,18 +1,8 @@
 package service
 
 import (
-	"errors"
 	"log"
 	"okusuri-backend/repository"
-)
-
-// カスタムエラー
-var (
-	ErrEmailAlreadyExists  = errors.New("このメールアドレスは既に使用されています")
-	ErrInvalidCredentials  = errors.New("メールアドレスまたはパスワードが無効です")
-	ErrUserNotFound        = errors.New("ユーザーが見つかりません")
-	ErrTokenGenerationFail = errors.New("トークン生成に失敗しました")
-	ErrInvalidToken        = errors.New("無効なトークンです")
 )
 
 // AuthService は認証関連の機能を提供する
