27ac2bd323ffaa2c87820ebf1e7f3109ccfad498
wato787 <wato787@icloud.com>
2025-04-13 18:14:58 +0900

    del

--- service/user_service.go
+++ service/user_service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"fmt"
-	"okusuri-backend/dto"
 	"okusuri-backend/model"
 	"okusuri-backend/repository"
 )
@@ -15,37 +14,6 @@ func NewUserService(userRepo *repository.UserRepository) *UserService {
 	return &UserService{
 		userRepo: userRepo,
 	}
-}
-
-// ユーザーを登録する
-func (s *UserService) RegisterUser(req dto.SignupRequest) (*model.User, error) {
-
-	// ユーザーの存在確認
-	existingUser, err := s.userRepo.FindByProviderId(req.ProviderID)
-	if err != nil {
-		return nil, err
-	}
-
-	if existingUser != nil {
-		return nil, nil // ユーザーは既に存在する
-	}
-
-	// 新しいユーザーの作成
-	newUser := &model.User{
-		Email:      req.Email,
-		Name:       req.Name,
-		ImageUrl:   req.ImageURL,
-		Provider:   model.ProviderGoogle,
-		ProviderId: req.ProviderID,
-	}
-
-	// ユーザーの保存
-	if err := s.userRepo.Create(newUser); err != nil {
-		return nil, err
-	}
-
-	return newUser, nil
-
 }
 
 // ユーザーをプロバイダーIDで取得する
