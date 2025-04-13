ad15576583f137da65f896b7aee06c78f5090219
wato787 <wato787@icloud.com>
2025-04-13 18:15:53 +0900

    del

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"log"
-	"okusuri-backend/helper"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -40,28 +39,4 @@ func CORS() gin.HandlerFunc {
 	}
 }
 
-// JWTAuth は認証を処理するミドルウェア
-func JWTAuth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		token := c.GetHeader("Authorization")
-
-		if token == "" {
-			c.JSON(401, gin.H{"error": "認証が必要です"})
-			c.Abort()
-			return
-		}
-
-		// トークンの検証
-		userId, err := helper.ValidateToken(token)
-		if err != nil {
-			c.JSON(401, gin.H{"error": "無効なトークン"})
-			c.Abort()
-			return
-		}
-
-		// 検証が成功した場合、ユーザー情報をコンテキストに設定
-		c.Set("userId", userId)
-
-		c.Next()
-	}
-}
+// TODO:tokenからユーザー情報を取得するミドルウェア作成
