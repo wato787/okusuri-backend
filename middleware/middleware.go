520ba666ca3f3455f2575f90ddaf1ebbc09fee87
wato787 <wato787@icloud.com>
2025-04-18 17:51:32 +0900

    ミドルウェア

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"log"
+	"okusuri-backend/repository"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -39,4 +40,31 @@ func CORS() gin.HandlerFunc {
 	}
 }
 
-// TODO:tokenからユーザー情報を取得するミドルウェア作成
+func Auth(userRepository *repository.UserRepository) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Bearerトークンを取得
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			c.JSON(401, gin.H{"error": "Authorization header is required"})
+			c.Abort()
+			return
+		}
+		token := authHeader[len("Bearer "):]
+		if token == "" {
+			c.JSON(401, gin.H{"error": "Token is required"})
+			c.Abort()
+			return
+		}
+		// sessionテーブルのtokenと一致するレコードを取得
+		user, err := userRepository.GetUserByToken(token)
+		if err != nil {
+			c.JSON(401, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+		// ユーザー情報をコンテキストに保存
+		c.Set("user", user)
+
+		c.Next()
+	}
+}
