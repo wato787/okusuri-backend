6eb30183d27cdf9a8c13c7fef2f868e98198d0de
wato787 <wato787@icloud.com>
2025-04-18 17:51:27 +0900

    ルート

--- routes/routes.go
+++ routes/routes.go
@@ -2,11 +2,15 @@ package routes
 
 import (
 	"okusuri-backend/middleware"
+	"okusuri-backend/repository"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes() *gin.Engine {
+
+	// userRepositoryを初期化
+	userRepository := repository.NewUserRepository()
 
 	// Ginのルーターを作成
 	router := gin.Default()
@@ -24,8 +28,8 @@ func SetupRoutes() *gin.Engine {
 		})
 
 		// 認証が必要なルートグループ
-		// secured := api.Group("/")
-		// secured.Use(middleware.JWTAuth())
+		secured := api.Group("/")
+		secured.Use(middleware.Auth(userRepository))
 		{
 			// ここに認証が必要なルートを追加
 			// 例: RegisterUserRoutes(secured, userController)
