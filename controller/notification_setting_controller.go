c133e98c0ea02f0780dd4fb32a5fda2c32b0e440
wato787 <wato787@icloud.com>
2025-04-19 01:53:20 +0900

    notifi

--- controller/notification_setting_controller.go
+++ controller/notification_setting_controller.go
@@ -14,6 +14,8 @@ import (
 type NotificationController struct {
 	NotificationRepository *repository.NotificationRepository
 	NotificationService    *service.NotificationService
+	UserRepository         *repository.UserRepository
+	UserService            *service.UserService
 }
 
 // NewNotificationController は新しいNotificationControllerのインスタンスを作成する
@@ -21,9 +23,13 @@ func NewNotificationController() *NotificationController {
 
 	notificationRepository := repository.NewNotificationRepository()
 	notificationService := service.NewNotificationService(notificationRepository)
+	userRepository := repository.NewUserRepository()
+	userService := service.NewUserService(userRepository)
 	return &NotificationController{
 		NotificationRepository: notificationRepository,
 		NotificationService:    notificationService,
+		UserRepository:         userRepository,
+		UserService:            userService,
 	}
 }
 
@@ -75,4 +81,41 @@ func (nc *NotificationController) RegisterNotificationSetting(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "notification setting registered successfully"})
+}
+
+// 通知送信
+func (nc *NotificationController) SendNotification(c *gin.Context) {
+	// ユーザーを全取得
+	users, err := nc.UserService.GetAllUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get users"})
+		return
+	}
+
+	// 通知設定を全取得
+	notificationSettings, err := nc.NotificationService.GetAllNotificationSettings()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get notification settings"})
+		return
+	}
+	// 通知設定をユーザーに紐づける
+	notificationSettingsMap := make(map[string]model.NotificationSetting)
+	for _, ns := range notificationSettings {
+		notificationSettingsMap[ns.UserID] = ns
+	}
+	// サービス層に通知送信処理を依頼
+	for _, user := range users {
+		notificationSetting, ok := notificationSettingsMap[user.ID]
+		if !ok {
+			continue
+		}
+		if notificationSetting.IsEnabled {
+			err := nc.NotificationService.SendNotification(user, notificationSetting, "お薬の時間です")
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to send notification"})
+				return
+			}
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "notification sent successfully"})
 }
