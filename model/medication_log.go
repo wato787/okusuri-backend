f392b6216b9b3390bb9169f14dd8a55cebd31423
wato787 <wato787@icloud.com>
2025-05-10 00:46:59 +0900

    fix typo

--- model/medication_log.go
+++ model/medication_log.go
@@ -1,10 +1,13 @@
 package model
 
-import "gorm.io/gorm"
+import "time"
 
 // 服用履歴の構造体
 type MedicationLog struct {
-	gorm.Model
-	UserID      string `json:"user_id" gorm:"not null;uniqueIndex:idx_user_id"`
-	HasBleeding bool   `json:"has_bleeding" gorm:"default:false"`
+	ID          uint       `json:"id" gorm:"primarykey"`
+	CreatedAt   time.Time  `json:"createdAt"`                        // 作成時に自動設定される
+	UpdatedAt   time.Time  `json:"updatedAt"`                        // 更新時に自動設定される
+	DeletedAt   *time.Time `json:"deletedAt,omitempty" gorm:"index"` // ソフトデリート用
+	UserID      string     `json:"userId" gorm:"not null;uniqueIndex:idx_user_id"`
+	HasBleeding bool       `json:"hasBleeding" gorm:"default:false"`
 }
--- model/notification_setting.go
+++ model/notification_setting.go
@@ -1,14 +1,15 @@
 package model
 
-import (
-	"gorm.io/gorm"
-)
+import "time"
 
 // ユーザーのFCM通知設定を管理する構造体
 type NotificationSetting struct {
-	gorm.Model
-	UserID    string `json:"user_id" gorm:"not null;uniqueIndex:idx_user_id"`
-	IsEnabled bool   `json:"is_enabled" gorm:"default:true"`
-	FcmToken  string `json:"fcm_token" gorm:"not null;uniqueIndex:idx_fcm_token"`
-	Platform  string `json:"platform" gorm:"not null;uniqueIndex:idx_platform"`
+	ID        uint       `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time  `json:"createdAt"`                        // 作成時に自動設定される
+	UpdatedAt time.Time  `json:"updatedAt"`                        // 更新時に自動設定される
+	DeletedAt *time.Time `json:"deletedAt,omitempty" gorm:"index"` // ソフトデリート用
+	UserID    string     `json:"userId" gorm:"not null;uniqueIndex:idx_user_id"`
+	IsEnabled bool       `json:"isEnabled" gorm:"default:true"`
+	FcmToken  string     `json:"fcmToken" gorm:"not null;uniqueIndex:idx_fcm_token"`
+	Platform  string     `json:"platform" gorm:"not null;uniqueIndex:idx_platform"`
 }
