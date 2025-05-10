ff73d4217f1ddc130802ded1da86b50e8566e253
wato787 <wato787@icloud.com>
2025-05-10 22:09:07 +0900

    不要箇所削除

--- dto/notification_setting.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type RegisterNotificationSettingRequest struct {
-	FcmToken  string `json:"fcmToken" binding:"required"`
-	IsEnabled bool   `json:"isEnabled" binding:"required"`
-	Platform  string `json:"platform" binding:"required"`
-}
