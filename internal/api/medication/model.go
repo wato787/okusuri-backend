6e7afa01a3f66a12740916889c29195542bb0019
wato787 <wato787@icloud.com>
2025-05-11 01:08:42 +0900

    index

--- internal/api/medication/model.go
+++ internal/api/medication/model.go
@@ -5,9 +5,9 @@ import "time"
 // 服用履歴の構造体
 type MedicationLog struct {
 	ID          uint       `json:"id" gorm:"primarykey"`
-	CreatedAt   time.Time  `json:"createdAt"`                        // 作成時に自動設定される
-	UpdatedAt   time.Time  `json:"updatedAt"`                        // 更新時に自動設定される
-	DeletedAt   *time.Time `json:"deletedAt,omitempty" gorm:"index"` // ソフトデリート用
-	UserID      string     `json:"userId" gorm:"not null;uniqueIndex:idx_user_id"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	UpdatedAt   time.Time  `json:"updatedAt"`
+	DeletedAt   *time.Time `json:"deletedAt,omitempty" gorm:"index"`
+	UserID      string     `json:"userId" gorm:"not null;index:idx_user_id"` // uniqueIndexからindexに変更
 	HasBleeding bool       `json:"hasBleeding" gorm:"default:false"`
 }
